a1762338021373820064759997537c5026b97a21
kosior <ksrk@protonmail.com>
2017-10-31 20:24:28 +0100

    add with_record, details methods

--- ufcroster/fighters/managers.py
+++ ufcroster/fighters/managers.py
@@ -4,3 +4,9 @@ from django.db import models
 class FighterQuerySet(models.QuerySet):
     def with_urls(self):
         return self.select_related('urls')
+
+    def with_record(self):
+        return self.select_related('record')
+
+    def details(self):
+        return self.select_related('record', 'urls')
