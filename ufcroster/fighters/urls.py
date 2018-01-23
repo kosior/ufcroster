df48558b3875a2476aaf4ae71b463a0ab3ab8a34
kosior <krzysiek.kosiorek@gmail.com>
2018-01-23 17:57:02 +0100

    add new views (country related) and CountryCodeMixin

--- ufcroster/fighters/urls.py
+++ ufcroster/fighters/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'fighters'
 
 urlpatterns = [
+    url(r'^(?P<country_code>[\w]{2})/$', views.UpcomingFightsByCountry.as_view(), name='upcoming-by-country'),
+    url(r'^(?P<country_code>[\w]{2})/fighters$', views.FightersByCountry.as_view(), name='by-country'),
     url(r'^(?P<slug>[\w\-]+)/$', views.FighterDetail.as_view(), name='detail'),
 ]
--- ufcroster/fighters/views.py
+++ ufcroster/fighters/views.py
@@ -1,9 +1,36 @@
-from django.views.generic import DetailView
+from django.conf import settings
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 
-from .models import Fighter
+from .models import Fighter, Fight, FightDetails
+
+
+class CountryCodeMixin:
+    def get_country_code(self):
+        country_code = self.kwargs.get('country_code', '')
+        if country_code in settings.COUNTRIES_URL_CODES:
+            return country_code.upper()
+        raise Http404
 
 
 class FighterDetail(DetailView):
     model = Fighter
     queryset = Fighter.objects.full_fighter()
     context_object_name = 'fighter'
+
+
+class UpcomingFightsByCountry(CountryCodeMixin, ListView):
+    context_object_name = 'fights'
+    template_name = 'fighters/upcoming_by_country.html'
+
+    def get_queryset(self):
+        return Fight.objects.filter(details__status=FightDetails.UPCOMING, fighter__country=self.get_country_code())
+
+
+class FightersByCountry(CountryCodeMixin, ListView):
+    context_object_name = 'fighters'
+    template_name = 'fighters/fighters.html'
+
+    def get_queryset(self):
+        in_ufc = 'released' not in self.request.GET.keys()
+        return Fighter.objects.filter(country=self.get_country_code(), in_ufc=in_ufc)
