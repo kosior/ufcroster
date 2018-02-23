fd4a676eea039df1cf4ef2a545bf22c94b3ad537
kosior <krzysiek.kosiorek@gmail.com>
2018-02-23 17:38:10 +0100

    fix celery config

--- config/celery.py
+++ config/celery.py
@@ -1,14 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import sys
 
+import django
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
+sys.path.append(os.path.join(str(settings.ROOT_DIR), 'ufcroster'))
+django.setup()
+
 app = Celery('ufcroster')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
