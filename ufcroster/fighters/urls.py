121a156ccd2b57316e440a5c08e99043bfca3341
kosior <krzysiek.kosiorek@gmail.com>
2018-03-01 13:55:01 +0100

    change "-" to "_" in url names

--- ufcroster/fighters/urls.py
+++ ufcroster/fighters/urls.py
@@ -6,7 +6,7 @@ app_name = 'fighters'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<country_code>[\w]{2})/$', views.UpcomingFightsByCountry.as_view(), name='upcoming-by-country'),
-    url(r'^(?P<country_code>[\w]{2})/fighters$', views.FightersByCountry.as_view(), name='by-country'),
+    url(r'^(?P<country_code>[\w]{2})/$', views.UpcomingFightsByCountry.as_view(), name='upcoming_by_country'),
+    url(r'^(?P<country_code>[\w]{2})/fighters$', views.FightersByCountry.as_view(), name='by_country'),
     url(r'^(?P<slug>[\w\-]+)/$', views.FighterDetail.as_view(), name='detail'),
 ]
