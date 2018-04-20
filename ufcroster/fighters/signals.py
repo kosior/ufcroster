4360a414c42a640dadad3fb104fd97ea2edbe0b2
kosior <ksrk@protonmail.com>
2018-04-20 13:27:49 +0200

    add signal on save for FighterRecord

--- ufcroster/fighters/signals.py
+++ ufcroster/fighters/signals.py
@@ -3,7 +3,7 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Fight
+from .models import Fight, FighterRecord
 from .utils import invalidate_fights_cache
 
 logger = logging.getLogger(__name__)
@@ -12,3 +12,8 @@ logger = logging.getLogger(__name__)
 @receiver(post_save, sender=Fight)
 def save_fight(sender, instance, **kwargs):
     invalidate_fights_cache([instance.fighter_id, instance.opponent_id])
+
+
+@receiver(post_save, sender=FighterRecord)
+def save_fight(sender, instance, **kwargs):
+    invalidate_fights_cache([instance.fighter_id])
