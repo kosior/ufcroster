4a7e198a889cf8d5c9aa8c64511d2950bcc866c2
kosior <ksrk@protonmail.com>
2018-04-20 16:43:09 +0200

    extend fighter_detail url

--- ufcroster/fighters/urls.py
+++ ufcroster/fighters/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<country_code>[\w]{2})/$', views.UpcomingFightsByCountry.as_view(), name='upcoming_by_country'),
     url(r'^(?P<country_code>[\w]{2})/fighters$', views.FightersByCountry.as_view(), name='by_country'),
-    url(r'^(?P<slug>[\w\-]+)/$', views.FighterDetail.as_view(), name='detail'),
+    url(r'^fighter/(?P<slug>[\w\-]+)/$', views.FighterDetail.as_view(), name='detail'),
 ]
